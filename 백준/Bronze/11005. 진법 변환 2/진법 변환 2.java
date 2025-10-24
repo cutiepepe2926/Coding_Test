2cf29f35ce76ea93ece28a04b5deded12e459336
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-10-24 16:20:07 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -5,28 +5,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        LinkedList<Long> LL = new LinkedList<>();
 
-        int N = Integer.parseInt(st.nextToken()); //10진수 숫자
-        int B = Integer.parseInt(st.nextToken()); //몇진법?
-
-        br.close();
-
-        ArrayList<Character> list = new ArrayList<>();
-
-        while (N > 0) { //몫이 0보다 클 경우(더이상 B진법으로 바꿀 수가 없을 경우)
-            if (N % B < 10) { //만약 나누었을때 나머지가 10보다 작을 경우
-                list.add((char) (N % B + '0')); //그냥 숫자출력
+        // 참고로 저장은 역순임!!!
+        for (long i=N;i>0;) {
+            if (i<B) {
+                LL.add(i);
+                break;
             }
-            else{ //10보다 크거나 같을 경우
-                /*예를 들어 16진수는 10부터 A이다. 즉 -10을 한담에 그 남은 횟수만큼 헤서 +'A'하면 구해야하는 B진법의
-                 숫자가 나옴*/
-                list.add((char) (N % B - 10 + 'A'));
-            }
-            N /= B; //B으로 나눔 => B진법의 한자리를 구했기 때문(여기서 한자리는 첫번째 자리가 아니라, 그냥 한칸을 의미)
+            long temp = i%B;
+            LL.add(temp);
+            i=i/B;
         }
 
-        for (int i = list.size() - 1; i >= 0; i--) { // 맨처음부터 출력해
-            System.out.print(list.get(i));
+
+        for (int i=LL.size()-1; i>=0; i--) {
+            if (LL.get(i)>9) {
+                System.out.print((char)(55+LL.get(i)));
+            }
+            else {
+                System.out.print(LL.get(i));
+            }
         }
     }
 }
+
