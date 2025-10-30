06593d8cfba05f9a4380c0a0f7172bd136eb94a4
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-10-30 17:35:56 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.08 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        
+        StringBuffer sb = new StringBuffer(my_string.length());
+        
+        for (int i=(my_string.length()-1); i>=0; i--) {
+            sb.append(my_string.charAt(i));
+        }
+        
+        String answer = sb.toString();
+    
+        return answer;
+    }
+}
