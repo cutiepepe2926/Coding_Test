c735e0dbb409a0dc5b8538427e79b961ded32467
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-11-05 15:00:04 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        for (int i=0; i<my_string.length(); i++) {
+            if (my_string.charAt(i) < 'A') {
+                answer += (my_string.charAt(i)-'0');
+            }
+        }
+        
+        return answer;
+    }
+}
