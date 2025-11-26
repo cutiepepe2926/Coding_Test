92965bed6d8e63f5f882ea663782507d40d554b1
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-11-27 00:13:40 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b) {
+        
+        if (a%2!=0 && b%2!=0) {
+            return a*a + b*b;
+        }
+        else if (a%2!=0 || b%2!=0 && !(a%2!=0 && b%2!=0) ) {
+            return 2 * (a+b);
+        }
+        else {
+            return Math.abs(a-b);
+        }
+    }
+}
