d3eb938ea1190068a7154112ccc63e20d51307aa
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-11-26 10:39:20 +0900

    Refactor countdown solution to use array instead of list

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,14 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        LinkedList<Integer> ll = new LinkedList<>();
         
-        for (int i=start_num; i>=end_num; i--) {
-            ll.add(i);
+        int[] answer = new int[start_num-end_num+1];
+        
+        for (int i=start_num, idx=0; i>=end_num; i--) {
+            answer[idx++] = i;
         }
         
-        int[] answer = ll.stream().mapToInt(x->x).toArray();
         return answer;
     }
 }
