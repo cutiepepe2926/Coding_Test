f0ff489172e862e902b44e0c4b6f40a607df23e3
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-11-27 13:23:54 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        
+        int answer = 0;
+        
+        
+        for (int i=0; i<3; i++) {
+            if (date1[i]==date2[i]) {
+                continue;
+            }
+            else {
+                answer = (date1[i]>date2[i])?0:1;
+                break;
+            }
+        }
+        
+        return answer;
+        
+    }
+}
