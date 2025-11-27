886ccd48e06cdba80b686aa417f4231b7990d550
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-11-27 11:40:16 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 13.46 ms, Memory: 153 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        for (int i=0; i<arr.length; i++) {
+            
+            if (arr[i]>=50 && arr[i]%2==0) {
+                arr[i] = arr[i]/2;
+                continue;
+            }
+            
+            if (arr[i]<50 && arr[i]%2!=0) {
+                arr[i] = arr[i]*2;
+                continue;
+            }
+        }
+        
+        int[] answer = arr;
+        
+        return answer;
+    }
+}
