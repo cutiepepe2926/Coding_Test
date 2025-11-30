e0eea5371f8e3d29e8400eddcded806a4785300d
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-11-30 14:46:17 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        String s = String.valueOf(n);
+        int answer = 0;
+        
+        for (int i = 0; i<s.length(); i++) {
+            answer += s.charAt(i)-'0';            
+        }
+
+        return answer;
+    }
+}
