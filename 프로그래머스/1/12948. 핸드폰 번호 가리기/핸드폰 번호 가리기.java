49a6bd95f63ab94a02ef8fe54e12c3a9eeb417c5
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-12-01 00:18:51 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(phone_number);
+        for (int i = 0; i < phone_number.length()-4; i++) {
+            sb.setCharAt(i, '*');
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
