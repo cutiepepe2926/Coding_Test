85a124d1b8ed594cffad8488cf5d703fe6aa90be
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-12-01 17:09:53 +0900

    Refactor number string replacements using array

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(String s) {
-        s = s.replaceAll("zero","0");
-        s = s.replaceAll("one","1");
-        s = s.replaceAll("two","2");
-        s = s.replaceAll("three","3");
-        s = s.replaceAll("four","4");
-        s = s.replaceAll("five","5");
-        s = s.replaceAll("six","6");
-        s = s.replaceAll("seven","7");
-        s = s.replaceAll("eight","8");
-        s = s.replaceAll("nine","9");
+        
+        String[] num = {"zero", "one", "two", "three", "four", "five","six","seven","eight","nine"};
+        for (int i = 0; i < num.length; i++) {
+            if (s.contains(num[i])) {
+                s = s.replaceAll(num[i],String.valueOf(i));
+            }
+        }
         int answer = Integer.parseInt(s);
         return answer;
     }
