9248512e2b03e97f6abdc95508cea3868c88e464
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-12-07 23:38:58 +0900

    [Bronze V] Title: Mathematics, Time: 96 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer += Integer.parseInt(br.readLine());
+        }
+        System.out.println(answer);
+    }
+}
