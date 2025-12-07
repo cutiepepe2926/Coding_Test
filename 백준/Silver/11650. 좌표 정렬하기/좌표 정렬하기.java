36cfd458e7bf67327ca95ec9e9775c0500f7dc07
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-12-07 23:35:01 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 636 ms, Memory: 50096 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Coordinate[] list = new Coordinate[n];
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            list[i] = new Coordinate(
+                Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())
+            );    
+        }
+        
+        Arrays.sort(list, (a,b) -> {
+            if (a.x == b.x) {
+                return Integer.compare(a.y, b.y);
+            }
+            return Integer.compare(a.x, b.x);
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (Coordinate l : list) {
+            sb.append(l.x).append(" ").append(l.y).append("\n");
+        }
+        
+        System.out.println(sb.toString());
+        
+    }
+    
+    public static class Coordinate {
+        int x;
+        int y;
+        
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
