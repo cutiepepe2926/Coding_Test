d123b2c9c8607a333326d27ff03531e8e75ccfd3
cutiepepe2926 <cutiepepe2926@gmail.com>
2026-01-01 21:05:50 +0900

    [Silver III] Title: 칸토어 집합, Time: 188 ms, Memory: 23328 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -11,33 +11,25 @@ public class Main {
             if (s == null) {
                 break;
             }
-//            if (s.equals("")) {
-//                break;
-//            }
             int n = Integer.parseInt(s);
             n = (int)Math.pow(3.0,n);
             //칸토어함수
-            canto(n, true);
+            canto(n);
             sb.append("\n");
         }
         System.out.println(sb);
     }
 
-    static void canto(int n, boolean y) {
-        if (n==0) {
+    static void canto(int n) {
+        if (n<=1) {
             sb.append("-");
             return;
         }
-        if (n==1 && y) {
-            sb.append("-");
-            return;
-        }
-        if (!y) {
-            sb.append(" ".repeat(n));
-            return;
-        }
-        canto(n/3, true);
-        canto(n/3, false);
-        canto(n/3, true);
+        
+        int three = n/3;
+        canto(three);
+        sb.append(" ".repeat(three));
+        canto(three);
+        
     }
 }
