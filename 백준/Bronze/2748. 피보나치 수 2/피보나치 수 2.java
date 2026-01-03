30a30f26a3bd8505d8949e358bbc984301810434
cutiepepe2926 <cutiepepe2926@gmail.com>
2026-01-03 19:32:05 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static long[] memo = new long[91];
+    
+    public static void main(String[] args) throws IOException {
+        Arrays.fill(memo, -1);
+        memo[0] = 0;
+        memo[1] = 1;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+    }
+    
+    static long fibonacci(int n) {
+        if (memo[n]!=-1) return memo[n];
+        return memo[n] = fibonacci(n-1) + fibonacci(n-2);
+    }
+    
+}
