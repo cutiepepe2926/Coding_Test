630247e66c69e822f45f32ca428d70a6ff0fbfa1
cutiepepe2926 <cutiepepe2926@gmail.com>
2026-01-03 19:03:00 +0900

    [Bronze II] Title: 피보나치 수, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -9,22 +9,6 @@ public class Main {
         Arrays.fill(memo, -1);
         memo[0] = 0;
         memo[1] = 1;
-        memo[2] = 1;
-        memo[3] = 2;
-        memo[4] = 3;
-        memo[5] = 5;
-        memo[6] = 8;
-        memo[7] = 13;
-        memo[8] = 21;
-        memo[9] = 34;
-        memo[10] = 55;
-        memo[11] = 89;
-        memo[12] = 144;
-        memo[13] = 233;
-        memo[14] = 377;
-        memo[15] = 610;
-        memo[16] = 987;
-        memo[17] = 1597;
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
