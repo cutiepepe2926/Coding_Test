234061aa1951cc028f7aa7ad9a36cc7a68ecec8b
cutiepepe2926 <cutiepepe2926@gmail.com>
2026-01-09 16:43:53 +0900

    [Gold IV] Title: 문자열 폭발, Time: 564 ms, Memory: 83304 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] s = br.readLine().toCharArray();
+        char[] bomb = br.readLine().toCharArray();
+        br.close();
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length; i++) {
+            stack.add(s[i]);
+            if (stack.peekLast() == bomb[bomb.length - 1]) {
+                boolean boom = true;
+                Deque<Character> temp = new ArrayDeque<>();
+                for (int j = bomb.length - 1; j >= 0; j--) {
+                    if (stack.isEmpty()) {
+                        boom = false;
+                        break;
+                    }
+                    char c = stack.pollLast();
+                    if (c == bomb[j]) {
+                        temp.add(c);
+                    }
+                    else {
+                        temp.add(c);
+                        boom = false;
+                        break;
+                    }
+                }
+
+                if (!boom) {
+                    while (!temp.isEmpty()) {
+                        stack.addLast(temp.pollLast());
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pollFirst());
+        }
+        System.out.println((sb.toString().isEmpty())?"FRULA":sb);
+    }
+}
