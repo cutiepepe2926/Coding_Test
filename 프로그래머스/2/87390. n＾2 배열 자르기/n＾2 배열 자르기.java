aaf114c92f5ae4f32f883114e72397e2285112ff
LEE GEON HEE <cutiepepe2926@gmail.com>
2026-04-04 10:38:04 +0900

    [level 2] Title: n^2 배열 자르기, Time: 2.93 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,26 +1,53 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, long left, long right) {
         
-        int len = (int)(right - left + 1);
-        int[] ans = new int[len];
+        long len = right - left + 1;
         
-        for (int i = 0; i < len; i++) {
-            long k = left + i;
-            long row = k / n;
-            long col = k % n;
-            ans[i] = (int)(Math.max(row,col) + 1);
+        int[] answer = new int[(int)len];
+        
+        int idx = 0;
+        
+        long si = left / n; // start i
+        long sj = left % n; // start j
+        long ei = right / n; // end i
+        long ej = right % n; // end j
+        
+        if (si == ei) {
+            for (long j = sj; j <= ej; j++) {
+                if (j <= si) answer[idx++] = (int)(si + 1);
+                else answer[idx++] = (int)(j + 1);
+            }
+            
+            return answer;
         }
         
-        return ans;
+        // sero
+        for (long i = si; i <= ei; i++) {
+            
+            // garo
+            if (i == si) { // start line
+                for (long j = sj; j < n; j++) { // 특정 시작지점부터 n까지
+                    if (j <= i) answer[idx++] = (int)(i + 1);
+                    else answer[idx++] = (int)(j + 1);
+                }
+            }
+            
+            else if (i == ei) { // end line
+                for (long j = 0; j <= ej; j++) { // 처음부터 끝지점(ej)까지
+                    if (j <= i) answer[idx++] = (int)(i + 1);
+                    else answer[idx++] = (int)(j + 1);
+                }
+            }
+            
+            else { // 한 줄 통째로
+                for (long j = 0; j < n; j++) {
+                    if (j <= i) answer[idx++] = (int)(i + 1);
+                    else answer[idx++] = (int)(j + 1);
+                }
+                
+            }
+        }
         
+        return answer;
     }
 }
-
-
-//12345
-//22345
-//33345
-//44445
-//55555
