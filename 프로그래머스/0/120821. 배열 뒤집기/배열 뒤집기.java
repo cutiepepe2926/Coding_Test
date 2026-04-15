4eaff1cf53c7088538b91e1afbbb79a34f65346c
LEE GEON HEE <cutiepepe2926@gmail.com>
2026-04-15 21:57:28 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.07 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,9 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list) {
+        
         int[] answer = new int[num_list.length];
-        for (int i=num_list.length-1,j=0; i>=0; i--,j++) {
-            answer[j] = num_list[i];
+        
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int nl : num_list) {
+            stack.push(nl);
         }
+        
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = stack.pop();
+        }
+        
         return answer;
+        
     }
 }
