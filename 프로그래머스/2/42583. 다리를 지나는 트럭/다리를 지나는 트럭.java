be222adbaa5f1337d94b933c93cfba6b6fcaad2c
LEE GEON HEE <cutiepepe2926@gmail.com>
2026-04-19 20:21:13 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 22.76 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -12,41 +12,28 @@ class Solution {
             bridge.offer(0);
         }
         
-        int time = 0; // 지난 시간
-        int curW = 0; // 현재 무게
-        int idx = 0; 
+        int time = 0;
+        int currentWeight = 0;
+        int idx = 0;
         
         // 아직 대기 트럭이 남아있거나, 다리 위에 트럭이 남아있으면 계속 진행
-        while (idx < truck_weights.length || curW > 0) {
-            
+        while (idx < truck_weights.length || currentWeight > 0) {
             time++;
             
             // 1초가 지나면서 맨 앞 칸이 빠져나감
-            curW -= bridge.poll();
+            currentWeight -= bridge.poll();
             
             // 다음 트럭을 올릴 수 있으면 올림
             if (idx < truck_weights.length) {
-                
-                if (curW + truck_weights[idx] <= weight) {
-                    
-                    // 트럭을 다리의 맨 뒤 칸에 올린다.
+                if (currentWeight + truck_weights[idx] <= weight) {
                     bridge.offer(truck_weights[idx]);
-                    
-                    // 현재 다리 위 총무게에 새 트럭 무게를 반영
-                    curW += truck_weights[idx];
-                    
-                    // 다음 트럭으로 인덱스 이동
+                    currentWeight += truck_weights[idx];
                     idx++;
-                
-                } 
-                
-                else {
+                } else {
                     // 못 올리면 빈 칸
                     bridge.offer(0);
                 }
-            } 
-            
-            else {
+            } else {
                 // 남은 트럭이 없으면 계속 빈 칸만 이동
                 bridge.offer(0);
             }
