a97f7ad6efe0a4ec89028392819b66de116bc754
LEE GEON HEE <cutiepepe2926@gmail.com>
2026-04-23 23:04:17 +0900

    [level 2] Title: 카펫, Time: 18.21 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -5,36 +5,17 @@ class Solution {
         
         int[] answer = new int[2];
         
-        ArrayList<int[]> al = new ArrayList<>();
-        
         for (int i = 1; i <= yellow; i++) {
+            
             if (yellow % i == 0) {
-                int[] temp = new int[2];
-                temp[0] = i;
-                temp[1] = yellow / i;
-                al.add(temp);
-            }
-        }
-        
-        ArrayList<int[]> candidate = new ArrayList<>();
-        
-        for (int[] t : al) {
-            if ( (t[0] + 2) * 2 + (t[1] * 2) == brown ) {
-                candidate.add(t);
-            }
-        }
-        
-        if (candidate.size() == 1) {
-            for (int[] t : candidate) {
-                answer[0] = t[0] + 2;
-                answer[1] = t[1] + 2;
-            }
-        }
-        else {
-            for (int[] t : candidate) {
-                if (t[0] >= t[1]) {
-                    answer[0] = t[0] + 2;
-                    answer[1] = t[1] + 2;
+                int x = i;
+                int y = yellow / i;
+                
+                if (2 * (x + y) + 4 == brown) {
+                    if (x >= y) {
+                        answer[0] = x + 2;
+                        answer[1] = y + 2;
+                    }
                 }
             }
         }
@@ -43,18 +24,3 @@ class Solution {
         
     }
 }
-
-// 8, 1 // 10, 2 // 12, 3 // 14, 4 // 16, 5 // 18, 6 // 20, 7 // 22, 8 // 24, 9 // 26, 10
-// 3, 3 // 4, 3 // 5, 3 //  6, 3 // 7, 3  // 8, 3   //  9, 3 // 10, 3 // 11, 3 //  12, 3
-
-// (y1 + 2) * 2 + (y2) * 2 = brwon
-// (1,2) (2,1) -> (6+4 = 10) (8 + 8 = 16)
-
-
-// yellow의 크기에 따라서 brown의 배치가 달라진다
-// yellow를 조져서 brown에 해당하는 값을 찾아야한다.
-// yellow = 24 
-// 1, 24 // 2, 12 // 3, 8 // 4, 6
-
-// yellow 쪼갠 값 y1, y2에 대해서 (y1 + 2) * 2 + (y2) * 2 = brown 를 만족하는 값을 찾아야 한다.
-// 그리고 나온 값에 대해서 (y1 >= y2) 여야하고 최종값인 [ y1 + 2, y2 + 2 ]가 곧 정답
