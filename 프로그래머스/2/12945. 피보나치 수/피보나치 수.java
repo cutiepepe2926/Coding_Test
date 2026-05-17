96555213d2eeace0b8ef03251fed005ec11a8b90
LEE GEON HEE <cutiepepe2926@gmail.com>
2026-05-17 11:40:05 +0900

    [level 2] Title: 피보나치 수, Time: 1.81 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,19 @@
 class Solution {
     public int solution(int n) {
-        long[] memo = new long[n+1];
+        
+        int[] memo = new int[100001];
+        
         memo[0] = 0;
         memo[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            memo[i] = (memo[i-1] + memo[i-2])%1234567;
+            
+            memo[i] = (memo[i-1] + memo[i-2]) % 1234567;
+            
         }
         
-        return (int)memo[n];
+        return memo[n];
+        
     }
+        
 }
