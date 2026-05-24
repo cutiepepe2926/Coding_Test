0ea57668e380d6daca7c854dd971f7e395465832
LEE GEON HEE <cutiepepe2926@gmail.com>
2026-05-24 11:37:57 +0900

    Time: 2 ms (99.29%), Space: 44.3 MB (65.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -34,4 +34,35 @@ class Solution {
                 c >= '0' && c <= '9');
 
     }
+
+    // StringBuilder sb = new StringBuilder();
+
+    // // 공백 및 알파벳과 숫자를 제외한 문제 제거
+    // for (int i = 0; i < s.length(); i++) {
+    //     if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z' || 
+    //         s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || 
+    //         s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+    //         sb.append(Character.toLowerCase(s.charAt(i)));
+    //     }
+    // }
+
+    // String palin = sb.toString();
+
+    // int left = 0;
+    // int right = palin.length() - 1;
+    
+    // boolean result = true;
+
+    // while (left <= right) {
+
+    //     if (palin.charAt(left) != palin.charAt(right)) {
+    //         result = false;
+    //         break;
+    //     }
+    //     left++;
+    //     right--;
+    // }
+    
+
+    // return result;
 }
