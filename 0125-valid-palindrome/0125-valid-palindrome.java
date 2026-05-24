2b7000712a3ad7a24ab81da0d8b531b922b0a3fb
LEE GEON HEE <cutiepepe2926@gmail.com>
2026-05-24 11:37:17 +0900

    Time: 2 ms (99.29%), Space: 44 MB (90.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,36 +1,37 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        StringBuilder sb = new StringBuilder();
-
-        // 공백 및 알파벳과 숫자를 제외한 문제 제거
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z' || 
-                s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || 
-                s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-                sb.append(Character.toLowerCase(s.charAt(i)));
-            }
-        }
-
-        String palin = sb.toString();
-
         int left = 0;
-        int right = palin.length() - 1;
-        
-        boolean result = true;
+        int right = s.length() - 1;
 
-        while (left <= right) {
+        while (left < right) {
 
-            if (palin.charAt(left) != palin.charAt(right)) {
-                result = false;
-                break;
+            while (left < right && !checkCase(s.charAt(left))) {
+                left++;
             }
+
+             while (left < right && !checkCase(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
             left++;
             right--;
-        }
-        
 
-        return result;
+        }
+
+        return true;
+
+    }
+
+    public boolean checkCase(char c) {
+        
+        return (c >= 'A' && c <= 'Z' || 
+                c >= 'a' && c <= 'z' || 
+                c >= '0' && c <= '9');
 
     }
 }
