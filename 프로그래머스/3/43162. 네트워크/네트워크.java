9a82db533fe0758ea41e15a7ce147273e652b31c
LEE GEON HEE <cutiepepe2926@gmail.com>
2026-05-29 20:04:46 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,44 @@
 class Solution {
     
-    static int x;
+    static boolean[] visited; // 방문체크용
     static int[][] network;
-    static boolean[] visited;
-    static void bfs(int start) {
-        visited[start] = true;
-        for (int v = 0; v < x; v++) {
-            if (network[start][v] == 1 && !visited[v]) {
-                bfs(v);
-            }
-        }
-    }
     
     public int solution(int n, int[][] computers) {
-        network = computers;
-        x=n;
+        
         int answer = 0;
         visited = new boolean[n];
+        network = computers;
+        
+        // 각 컴퓨터를 순회
         for (int i = 0; i < n; i++) {
+        
+            // 이미 방문했다면 무시
             if (!visited[i]) {
-                bfs(i);
+                
+                // dfs 순회로 연결된 네트워크 탐지
+                dfs(i);
+                
                 answer++;
+            } 
+            
+        }
+
+        return answer;
+        
+    }
+    
+    static void dfs(int node) {
+        
+        // 해당 노드 방문 체크
+        visited[node] = true;
+        
+        // 연결된 만큼 방문 + 방문여부 체크
+        for (int i = 0; i < network.length; i++) {
+            if (network[node][i] == 1 && !visited[i]) {
+                dfs(i);
             }
         }
-        return answer;
+        
     }
+
 }
